28c8066ee9096228c7c9e20066cb1ed9c04a861e
kamba <rok.kambic@z4g4.com>
2025-09-21 14:46:16 +0200

    c

--- fish/config.fish
+++ fish/config.fish
@@ -11,14 +11,14 @@ neofetch
 eval (starship init fish)
 
 # Start atuin
-atuin init fish | source
+# atuin init fish | source
 
 # List Directory
-abbr l 'eza -lh  --icons=auto --hyperlink' # long list
-abbr ls 'eza -1   --icons=auto --hyperlink' # short list
-abbr ll 'eza -lha --icons=auto --sort=name --group-directories-first --hyperlink' # long list all
-abbr ld 'eza -lhD --icons=auto --hyperlink' # long list dirs
-abbr lt 'eza --icons=auto --tree --hyperlink' # list folder as treabbr df "df -T -h"
+alias l='eza -lh  --icons=auto --hyperlink' # long list
+alias ls= 'eza -1   --icons=auto --hyperlink' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first --hyperlink' # long list all
+alias ld='eza -lhD --icons=auto --hyperlink' # long list dirs
+alias lt='eza --icons=auto --tree --hyperlink' # list folder as treabbr df "df -T -h"
 
 abbr hashed_password "openssl passwd -apr1"
 abbr lsblk "lsblk -f"
@@ -26,7 +26,7 @@ abbr lsblk "lsblk -f"
 abbr ip 'ip -c'
 abbr lsusb cyme
 
-abbr s 'kitten ssh'
+alias s='kitten ssh'
 abbr update-grub 'sudo grub-mkconfig -o /boot/grub/grub.cfg'
 abbr cze 'chezmoi edit'
 abbr cza 'chezmoi add'
