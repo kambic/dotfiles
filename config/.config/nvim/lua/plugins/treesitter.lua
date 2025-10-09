80807748f819ff79d73a5c68342822bde12b65cd
kamba <rok.kambic@z4g4.com>
2025-10-10 01:19:01 +0200

    up

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,37 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  lazy = false, -- keep it loaded on startup for consistent highlighting
+  priority = 100, -- make sure it loads early
   config = function()
-    -- local configs = require("nvim-treesitter.configs")
-    -- configs.setup({
-    --     ensure_installed = {"lua", "c", "javascript"},
-    --     highlight = { enable = true },
-    --     indent = { enable = true }
-    -- })
-    --
+    local ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not ok then
+      vim.notify("nvim-treesitter not found", vim.log.levels.ERROR)
+      return
+    end
+
+    configs.setup({
+      ensure_installed = {
+        "lua",
+        "c",
+        "javascript",
+        "python",
+        "bash",
+        "json",
+        "yaml",
+        "html",
+        "css",
+        "vim",
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    })
   end,
 }
