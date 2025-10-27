f268eb2d4c6b5b0b5c85eafcfa5b8bb198f0bfc0
rok <rok.kambic@mx.si>
2025-10-27 19:07:44 +0100

    kitty

--- core/.config/nvim-modular/lua/lazy-bootstrap.lua
+++ core/.config/nvim-modular/lua/lazy-bootstrap.lua
@@ -1,46 +1,77 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    lazyrepo,
+    lazypath,
+  }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
 end
 
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
+vim.opt.rtp:prepend(lazypath)
+-------------------------------------------------------
+-- 🚀 Plugin setup via Lazy.nvim
+-------------------------------------------------------
 require('lazy').setup({
+
+  -- Indent detection
   {
-    'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+    'NMAC427/guess-indent.nvim',
     opts = {
-      auto_cmd = true, -- auto-detect on FileType event
+      auto_cmd = true,
       override_editorconfig = true,
     },
   },
 
+  -----------------------------------------------------
+  -- 🔔 Notification system
+  -----------------------------------------------------
+  {
+    'rcarriga/nvim-notify',
+    lazy = false,
+    config = function()
+      local notify = require 'notify'
+      notify.setup {
+        stages = 'fade_in_slide_out',
+        timeout = 3000,
+        background_colour = '#1a1b26',
+        render = 'default',
+        top_down = false,
+      }
+      vim.notify = notify
+    end,
+  },
+
+  -----------------------------------------------------
+  -- 🧩 Your other plugin imports
+  -----------------------------------------------------
   { import = 'plugins' },
   require 'extra.lualine',
   require 'extra.bufferline',
-  require 'extra.noice',
-
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
+  -- require("extra.notify"),
 }, {
+  -----------------------------------------------------
+  -- ⚙️ Lazy.nvim behavior
+  -----------------------------------------------------
+  defaults = {
+    lazy = true,
+    version = false,
+  },
   install = {
-    -- install missing plugins on startup. This doesn't increase startup time.
+    -- Automatically install missing plugins
     missing = true,
-    -- try to load one of these colorschemes when starting an installation during startup
+    -- Automatically set colorscheme
     -- colorscheme = { 'onedark' },
   },
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
@@ -59,5 +90,8 @@ require('lazy').setup({
   },
 })
 
--- vim: ts=2 sts=2 sw=2 et
+-------------------------------------------------------
+-- ✅ Safety fallback if theme fails
+-------------------------------------------------------
+-- pcall(vim.cmd.colorscheme, theme)
 -- vim: ts=2 sts=2 sw=2 et
