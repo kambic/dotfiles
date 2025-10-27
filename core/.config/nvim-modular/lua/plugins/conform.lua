1df2a37e7882250bd514c61c2c1204b5368d519e
rok <rok.kambic@mx.si>
2025-10-27 22:35:49 +0100

    vim

--- core/.config/nvim-modular/lua/plugins/conform.lua
+++ core/.config/nvim-modular/lua/plugins/conform.lua
@@ -19,6 +19,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
+        vim.notify 'Formatting on save'
         local disable_filetypes = { c = true, cpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
@@ -31,14 +32,12 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
         bash = { 'beautysh' },
         sh = { 'beautysh' },
         css = { 'prettier' },
         markdown = { 'prettier' },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
+        json = { 'prettier', 'biome' },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
