1173bc4975f61170c13b552bfdf384226eb54135
Rok Kambič <33936550+kambic@users.noreply.github.com>
2026-01-20 15:55:40 +0100

    Refactor Treesitter configuration to use opts

--- home/configuration/nvim/lua/plugins/treesitter.lua
+++ home/configuration/nvim/lua/plugins/treesitter.lua
@@ -3,13 +3,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
     build = ":TSUpdate",
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = require("user.languages").parsers,
-        highlight = { enable = true },
-        indent = { enable = true },
-      }
-    end,
+    opts = {
+      ensure_installed = require("user.languages").parsers,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
   },
 }
