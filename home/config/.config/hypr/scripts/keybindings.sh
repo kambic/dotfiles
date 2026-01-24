81840de99715baa622fc9715067da9cb6e5d9aa5
rok <rok.kambic@gmail.com>
2026-01-24 15:09:45 +0100

    new hypr

--- home/config/.config/hypr/scripts/keybindings.sh
+++ home/config/.config/hypr/scripts/keybindings.sh
@@ -1,38 +1,13 @@
 #!/bin/bash
-#  _              _     _           _ _
-# | | _____ _   _| |__ (_)_ __   __| (_)_ __   __ _ ___
-# | |/ / _ \ | | | '_ \| | '_ \ / _` | | '_ \ / _` / __|
-# |   <  __/ |_| | |_) | | | | | (_| | | | | | (_| \__ \
-# |_|\_\___|\__, |_.__/|_|_| |_|\__,_|_|_| |_|\__, |___/
-#           |___/                             |___/
-# -----------------------------------------------------
+# Format JSON proper;y
+JSON=$(hyprkeys --from-ctl --json | jq -r --slurp "[.[]][0]");
 
-config_file=~/.config/hypr/binds.conf
-echo "Reading from: $config_file"
+USER_SELECTED=$(echo $JSON | jq -r '.[] | "\(.mods) \(.key) \(.dispatcher) \(.arg)"' | wofi --dmenu -p 'Keybinds' --define "dmenu-print_line_num=true");
 
-keybinds=""
+if [ -z "$USER_SELECTED" ]; then
+    exit 0;
+fi
 
-# Detect Start String
-while read -r line
-do
-    if [[ "$line" == "bind"* ]]; then
+EVENT=$(echo $JSON | jq -r "[.[]] | .[$USER_SELECTED]" | jq -r '"\(.dispatcher) \(.arg)"');
 
-        line="$(echo "$line" | sed 's/$mainMod/SUPER/g')"
-        line="$(echo "$line" | sed 's/bind = //g')"
-        line="$(echo "$line" | sed 's/bindm = //g')"
-
-        IFS='#'
-        read -a strarr <<<"$line"
-        kb_str=${strarr[0]}
-        cm_str=${strarr[1]}
-
-        IFS=','
-        read -a kbarr <<<"$kb_str"
-
-        item="${kbarr[0]}  + ${kbarr[1]}"$'\r'"${cm_str:1}"
-        keybinds=$keybinds$item$'\n'
-    fi
-done < "$config_file"
-
-sleep 0.2
-rofi -dmenu -i -markup -eh 2 -replace -p "Keybinds" <<< "$keybinds"
+hyprctl dispatch "$EVENT";
