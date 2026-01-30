64c3e61179a85227c21090dca4774e0c272ed60f
Rok Kambic <rok.kambic@gmail.com>
2026-01-30 20:07:26 +0100

    up

---
+++ glances.md
@@ -0,0 +1,1 @@
+sudo setcap cap_net_raw,cap_net_admin,cap_sys_ptrace,cap_dac_read_search+ep $(which glances)
--- home/config/.config/fish/config.fish
+++ home/config/.config/fish/config.fish
@@ -1,30 +1,3 @@
-### EXPORT ###
-set fish_greeting                                 # Supresses fish's intro message
-
-
-# if status is-interactive
-# 		set POSH agnoster
-# 		oh-my-posh init -c ~/.config/ohmyposh/zen.toml fish | source
-# # -----------------------------------------------------
-# end
-
-
-### RANDOM COLOR SCRIPT ###
 if status is-interactive
-    colorscript exec suckless
+# Commands to run in interactive sessions can go here
 end
-
-### SETTING THE STARSHIP PROMPT ###
-starship init fish | source
-
-for file in ~/.config/fish/conf.d/*.fish
-    source $file
-end
-
-### FZF ###
-# Enables the following keybindings:
-# CTRL-t = fzf select
-# CTRL-r = fzf history
-# ALT-c  = fzf cd
-fzf --fish | source
-umask 002
